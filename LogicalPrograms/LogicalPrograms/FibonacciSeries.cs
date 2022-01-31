017c08bca2ab46c70f164aa746fc5bf41431a28d
Manish <manishkumar0156@gmail.com>
2022-01-31 15:06:58 +0530

    [Manish] Add: Fabonacci Series problem

---
+++ LogicalPrograms/LogicalPrograms/FibonacciSeries.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class FibonacciSeries
+    {
+        public void Series()
+        {
+            int firstNum=1, secondNum=1;
+            int thirdNum;
+
+            Console.WriteLine(firstNum + "\t" + secondNum);
+            for(int i=3; i<=10; i++)
+            {
+                thirdNum = firstNum + secondNum;
+                firstNum = secondNum;
+                secondNum = thirdNum;
+
+                Console.Write("\t"+ thirdNum);
+            }
+        }
+    }
+}
---
+++ LogicalPrograms/LogicalPrograms/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 1. to print Fabonacci Series \n");
+
+            bool flag = true;
+            while (flag)
+            {
+                int check = Convert.ToInt32(Console.ReadLine());
+
+                switch (check)
+                {
+                    case 1:
+                        FibonacciSeries fs = new FibonacciSeries();
+                        fs.Series();
+                        break;
+
+
+                   
+                   
+
+                    
+                      
+
+                  
+                }
+            }
+
+        }
+    }
+}
